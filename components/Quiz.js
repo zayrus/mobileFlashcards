0ce58ebc581de49f068567cdbb3288f0b5887ed0
Ernest <zayrus@gmail.com>
2018-02-21 08:05:47 -0300

    feat(screen quiz): add quiz
    add quiz scaffolding

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,5 +1,36 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { Component } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { green, red, white, purple } from '../utils/colors'
+import { connect } from 'react-redux'
 
-export default class Quiz extends React.Component {
+class Quiz extends Component {
+
+  state = {
+    currentIndex: 0,
+    right: 0,
+    showAnswer: false
+  }
+
+  render() {
+    const questions = navigation.state.params.questions
+    const { currentIndex, showAnswer } = this.state
+
+    return (
+			
+      <View style={{flex:1, backgroundColor:white}}>
+
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    alignItems: 'center'
+  }
+})
+
+
+export default connect()(Quiz)
